0124f86287b80bf0cda64c34532860a7db5a9b5a
Meysam Agah <113939902+MeysamAgah@users.noreply.github.com>
2025-01-07 00:02:15 +0330

    Update Scraper.py

--- Codes/Scraper.py
+++ Codes/Scraper.py
@@ -1,3 +1,34 @@
+!pip install selenium
+!apt-get install chromium-driver
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+
+user_agent_string = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'
+
+def web_driver():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--verbose")
+    options.add_argument('--no-sandbox')
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument("--window-size=1920, 1200")
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument(f"user-agent={user_agent_string}")
+    driver = webdriver.Chrome(options=options)
+    return driver
+
+driver = web_driver()
+
+url = 'https://www.metacritic.com/game/elden-ring/user-reviews/?platform=playstation-5'
+
+driver.get(url)
+
+
 quotes = []
 while True:
     # Scroll to the bottom of the page
