62b0b883cfe0650ce3033da408cb778ccf8a2daa
Meysam Agah <113939902+MeysamAgah@users.noreply.github.com>
2025-01-30 23:52:35 +0330

    Create scraper.py

---
+++ scraper.py
@@ -0,0 +1,12 @@
+def web_driver(user_agent_string = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'):
+    options = webdriver.ChromeOptions()
+    options.add_argument("--verbose")
+    options.add_argument('--no-sandbox')
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument("--window-size=1920, 1200")
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument(f"user-agent={user_agent_string}")
+    driver = webdriver.Chrome(options=options)
+    return driver
+
